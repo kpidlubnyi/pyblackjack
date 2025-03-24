08d529efb50dda081477065aab2117e9dde1d7df
kostyapidlubnyi <kpidlubnyi@gmail.com>
2025-03-24 22:24:04 +0100

    added the Game class, implemented basic game logic

--- Classes.py
+++ Classes.py
@@ -1,5 +1,6 @@
 from itertools import product
 from random import choice, shuffle, randint
+from time import sleep
 
 
 class Card:
@@ -100,7 +101,10 @@ class Dealer(Hand):
 
     def __str__(self):
         result = f'{'  '.join([f'{card}' for card in self.cards])}'
-        result += f"\n   Total: {self.cards[0].value}+" if self.cards[1].hidden else f"\n   Total: {abs(self)}"
+        try:
+            result += f"\n   Total: {self.cards[0].value}+" if self.cards[1].hidden else f"\n   Total: {abs(self)}"
+        except IndexError:
+            result += f"\n   Total: {abs(self)}"
         return result
 
     def pick_card_from(self, deck: FrenchDeck):
@@ -129,21 +133,7 @@ class Table:
         return f'Table[deck: {len(self.deck)}, dealer: {self.dealer!r}, player: {self.player!r}]'
 
     def __str__(self):
-        return f"""
-{'='*60}                                          
-{'BlackJack Table':^60}
-{'='*60}
-   Dealer\'s Hand:{f'Cards remaining: {str(len(self.deck))}':>{55 - len('   Dealer\'s Hand:')}}
-{str(self.dealer):^30}
-
-
-
-   Your Hand:
-{str(self.player):^30}
-{'='*60}
-{'[HIT(1)]                 [STAND(2)]':^60}
-{'='*60}
-"""
+        return self.__repr__()
 
     def change_deck(self):
         self.deck = FrenchDeck() + FrenchDeck() + FrenchDeck() + FrenchDeck()
@@ -152,11 +142,140 @@ class Table:
 
     def player_pick(self):
         self.player.pick_card_from(self.deck)
+        self.golden_card += 1
 
     def dealer_pick(self):
         self.dealer.pick_card_from(self.deck)
+        self.golden_card += 1
 
     def show_dealers_card(self):
         self.dealer.show_second_card()
 
 
+def is_smaller_21(n: int):
+    return False if n > 21 else True
+
+
+class Game:
+    def __init__(self, balance: int = 500):
+        self.deck = FrenchDeck() + FrenchDeck() + FrenchDeck() + FrenchDeck()
+        self.deck.shuffle_deck()
+        self.balance = balance
+        self.player = Player()
+        self.dealer = Dealer()
+        self.table = Table(self.dealer, self.player)
+        self.bet = 0
+        self.is_still_on = True
+
+        print("WELCOME TO PYBLACKJACK!")
+        sleep(3)
+
+    def __repr__(self):
+        return f'Game[deck: {self.deck}, balance: {self.balance}, player: {self.player!r}, dealer: {self.dealer!r}]'
+
+    def __str__(self):
+        return f"""
+{'=' * 60}                                          
+{f'     Bet: {self.bet}'}{'BlackJack Table':^38}{f'[ {f'{self.balance}'} ]'}
+{'=' * 60}
+   Dealer\'s Hand:{f'Cards remaining: {str(len(self.deck))}':>{55 - len('   Dealer\'s Hand:')}}
+{str(self.dealer):^30}
+
+
+
+   Your Hand:
+{str(self.player):^30}
+{'=' * 60}
+{'[HIT(1)]                 [STAND(2)]':^60}
+{'=' * 60}
+"""
+
+    def second_card_is_hidden(self):
+        return self.dealer.cards[1].hidden
+
+    def start_round(self):
+        self.is_still_on = True
+
+        print(self)
+        sleep(2)
+
+        turns = [self.player, self.dealer] * 2
+        for hand in turns:
+            hand.pick_card_from(self.deck)
+            print(self)
+            sleep(0.5)
+
+        if abs(self.dealer) == 21:
+            if self.second_card_is_hidden():
+                self.dealer.show_second_card()
+            self.lose_round()
+
+    def summarize_round(self):
+        if abs(self.player) > abs(self.dealer):
+            self.win_round()
+        elif abs(self.dealer) > abs(self.player):
+            self.lose_round()
+        else:
+            self.draw()
+
+    def dealers_turn(self):
+        self.dealer.show_second_card()
+
+        while abs(self.dealer) < 17:
+            self.dealer.pick_card_from(self.deck)
+            print(self)
+
+        if abs(self.dealer) > 21:
+            self.win_round()
+        else:
+            self.summarize_round()
+
+    def betting(self):
+        while True:
+            amount = int(input("Enter the bet amount: "))
+            if self.bet_is_valid(amount):
+                break
+        self.bet += amount
+        self.balance -= amount
+        self.start_round()
+
+    def bet_is_valid(self, amount: int):
+        return True if amount <= self.balance and amount % 10 == 0 else False
+
+    def hit(self):
+        self.player.pick_card_from(self.deck)
+        print(self)
+        if not is_smaller_21(abs(self.player)):
+            self.lose_round()
+        elif abs(self.player) == 21:
+            self.win_round()
+        else:
+            pass
+
+    def stand(self):
+        self.dealers_turn()
+
+    def clean_table(self):
+        self.bet = 0
+        self.player.cards = []
+        self.dealer.cards = []
+
+    def lose_round(self):
+        print('YOU LOSE!')
+        print(self)
+        self.clean_table()
+        self.is_still_on = False
+
+    def win_round(self):
+        print('YOU WIN!')
+        print(self)
+        self.balance += 2 * self.bet
+        self.clean_table()
+        self.is_still_on = False
+
+    def draw(self):
+        print('DRAW!')
+        print(self)
+        self.balance += self.bet
+        self.clean_table()
+        self.is_still_on = False
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from Classes import Player, Dealer, Table
+from Classes import Game
 
-DEALER_TURN = False
+game = Game()
+
+while game.balance >= 20:
+    game.betting()
+
+    while game.is_still_on:
+        while True:
+            action = int(input("Choose your action: "))
+            if action not in (1, 2):
+                print("ONLY 1 OR 2")
+            else:
+                break
+
+        match action:
+            case 1:
+                game.hit()
+            case 2:
+                game.stand()
+    print("IT'S OVER OF THE ROUND")
+print("SEEMS LIKE YOU DON'T HAVE EVEN PENNY! SEE YOU LATER!")
 
 
-def build_game():
-    player = Player()
-    dealer = Dealer()
-    table = Table(dealer, player)
-
-    return table
-
-
-game = build_game()
-
-game.player_pick()
-game.player_pick()
-
-game.dealer_pick()
-game.dealer_pick()
-
-print(game)
